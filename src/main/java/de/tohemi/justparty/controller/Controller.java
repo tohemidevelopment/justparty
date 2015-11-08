849f046e0803cc13b85dd94171d473398f111764
Tom433 <tom@wolske.tv>
2015-11-08 12:25:02 +0100

    add addressourcehandlers method

--- src/main/java/de/tohemi/justparty/controller/Controller.java
+++ src/main/java/de/tohemi/justparty/controller/Controller.java
@@ -1,7 +1,18 @@
 package main.java.de.tohemi.justparty.controller;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 /**
  * Created by Heiko on 04.11.2015.
  */
-public class Controller {
+@Configuration
+@EnableWebMvc
+public class Controller extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 }
