0c070efa6e692ece94653a4eb5d57ae53e053384
Tom433 <tom@wolske.tv>
2015-11-09 11:48:48 +0100

    add mysql database to project

--- src/main/java/de/tohemi/justparty/controller/Controller.java
+++ src/main/java/de/tohemi/justparty/controller/Controller.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class Controller extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
-    }
+    registry.addResourceHandler("/resources/**")
+            .addResourceLocations("/resources/");
+}
 }
