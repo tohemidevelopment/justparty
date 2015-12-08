d69475ecbd7ad9b4482cea814b38d2ecd826db58
Heiko Faller <heiko_faller@t-online.de>
2015-12-08 13:20:55 +0100

    refactor

--- src/main/java/de/tohemi/justparty/database/tables/GuestlistDBTabelle.java
+++ src/main/java/de/tohemi/justparty/database/tables/GuestlistDBTabelle.java
@@ -11,15 +11,18 @@ public class GuestlistDBTabelle {
     public static final String COLUMN_STATUS = "status";
     public static final String COLUMN_EVENT = "event";
     public static final String COLUMN_GUEST = "guest";
+    public static final int ACEPTED=1;
+    public static final int DECLINED=2;
+    public static final int NOTSURE=3;
 
 
     public static Accepted getAcceptedObjectForStatus(int status) {
         switch (status) {
-            case 1:
+            case ACEPTED:
                 return Accepted.ACCEPTED;
-            case 2:
+            case DECLINED:
                 return Accepted.DECLINED;
-            case 3:
+            case NOTSURE:
                 return Accepted.NOTSURE;
         }
         return null;
