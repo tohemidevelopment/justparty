6d44e4567af0525d15ea63745baf6d9f8c276219
tom@wolske.tv <wolske123>
2016-04-25 11:51:59 +0200

    - create Declaration model

---
+++ src/main/java/de/tohemi/justparty/datamodel/Declaration.java
@@ -0,0 +1,50 @@
+package de.tohemi.justparty.datamodel;
+
+/**
+ * Created by xce35l2 on 25.04.2016.
+ */
+public class Declaration {
+    private String name;
+    private User user;
+    private boolean bringWithByAll;
+    private int event_id;
+
+    public Declaration(String name, User user, boolean bringWithByAll, int event_id){
+        this.bringWithByAll = bringWithByAll;
+        this.event_id = event_id;
+        this.name = name;
+        this.user = user;
+    }
+
+    public int getEvent_id() {
+        return event_id;
+    }
+
+    public void setEvent_id(int event_id) {
+        this.event_id = event_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isBringWithByAll() {
+        return bringWithByAll;
+    }
+
+    public void setBringWithByAll(boolean bringWithByAll) {
+        this.bringWithByAll = bringWithByAll;
+    }
+}
