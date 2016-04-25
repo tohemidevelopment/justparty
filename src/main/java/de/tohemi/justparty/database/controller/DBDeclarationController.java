1d95c0de31eaab8939537832856bb5463551b5d9
tom@wolske.tv <wolske123>
2016-04-25 11:39:22 +0200

    - update DBDeclarationsController

--- src/main/java/de/tohemi/justparty/database/controller/DBDeclarationController.java
+++ src/main/java/de/tohemi/justparty/database/controller/DBDeclarationController.java
@@ -42,50 +42,17 @@ public class DBDeclarationController {
         }
     }
 
-    public boolean createDeclarationTableForEvent(Event e) {
-
-        DataSource ds = getDataSource();
-        Connection c = DataSourceUtils.getConnection(ds);
-        try {
-            PreparedStatement ps = c.prepareStatement("CREATE TABLE declaration_" + e.getId() + " ( id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, usertobringwith VARCHAR(255) NOT NULL, bringwithbyall BOOL DEFAULT 0 NOT NULL );");
-            ps.execute();
-            ps.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            return false;
-        } finally {
-            releaseConnection(ds, c);
-        }
-        return true;
-    }
-
-    public boolean deleteDeclarationTableForEvent(Event e) {
-
-        DataSource ds = getDataSource();
-        Connection c = DataSourceUtils.getConnection(ds);
-        try {
-            PreparedStatement ps = c.prepareStatement("DROP TABLE declaration_" + e.getId() + ";");
-            ps.execute();
-            ps.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            return false;
-        } finally {
-            releaseConnection(ds, c);
-        }
-        return true;
-    }
-
     public boolean addDeclaration(Event e, Declaration d) {
 
         DataSource ds = getDataSource();
         Connection c = DataSourceUtils.getConnection(ds);
         try {
-                PreparedStatement ps = c.prepareStatement("INSERT INTO declaration_" + e.getId() + "(name, usertobringwith, bringwithbyall) VALUES (?, ?, ?);");
+                PreparedStatement ps = c.prepareStatement("INSERT INTO declaration(name, usertobringwith, bringwithbyall, event_id) VALUES (?, ?, ?, ?);");
                 ps.setString(1,d.getProperty());
                 ps.setString(2, d.getParsedValue().toString());
                 //TODO: Get True: FORALL False: only one from declaration
                 ps.setBoolean(3, false);
+                ps.setInt(4, e.getId());
                 ps.execute();
                 ps.close();
 
@@ -103,11 +70,12 @@ public class DBDeclarationController {
         DataSource ds = getDataSource();
         Connection c = DataSourceUtils.getConnection(ds);
         try {
-                PreparedStatement ps = c.prepareStatement("DELETE * FROM declaration_" + e.getId() + " WHERE name=? AND usertobringwith=? AND bringwithbyall=?;");
+                PreparedStatement ps = c.prepareStatement("DELETE * FROM declaration WHERE name=? AND usertobringwith=? AND bringwithbyall=? AND event_id=?;");
                 ps.setString(1,d.getProperty());
                 ps.setString(2, d.getParsedValue().toString());
                 //TODO: Get True: FORALL False: only one from declaration
                 ps.setBoolean(3, false);
+                ps.setInt(4, e.getId());
                 ps.execute();
                 ps.close();
 
@@ -125,12 +93,12 @@ public class DBDeclarationController {
         DataSource ds = getDataSource();
         Connection c = DataSourceUtils.getConnection(ds);
         try {
-            PreparedStatement ps = c.prepareStatement("UPDATE declaration_" + e.getId() + " SET name=?, usertobringwith=?, bringwithbyall=? WHERE id=?;");
+            PreparedStatement ps = c.prepareStatement("UPDATE declaration SET name=?, usertobringwith=?, bringwithbyall=? WHERE id=?;");
             ps.setString(1,d.getProperty());
             ps.setString(2, d.getParsedValue().toString());
             //TODO: Get True: FORALL False: only one from declaration
             ps.setBoolean(3, false);
-            //ps.setInt(4,d.getID());
+            ps.setInt(4,e.getId());
             ps.execute();
             ps.close();
 
@@ -148,7 +116,8 @@ public class DBDeclarationController {
         Connection c = DataSourceUtils.getConnection(ds);
         ArrayList<Declaration> declaration = new ArrayList<Declaration>();
         try {
-            PreparedStatement ps = c.prepareStatement("Select * From declaration_" + e.getId() + ";");
+            PreparedStatement ps = c.prepareStatement("Select * From declaration WHERE event_id=?;");
+            ps.setInt(1, e.getId());
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
                 //TODO: CHange RS:GETSTRING("Usertobringwith") to PARSEVALUEOF
