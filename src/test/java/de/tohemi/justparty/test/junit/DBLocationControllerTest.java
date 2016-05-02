66be63eee92542ff34f6b2ac39afe985175781d9
Tom433 <tom@wolske.tv>
2016-05-02 15:26:13 +0200

    - implement tests   - delete old testcases

--- src/test/java/de/tohemi/justparty/test/junit/DBLocationControllerTest.java
+++ src/test/java/de/tohemi/justparty/test/junit/DBLocationControllerTest.java
@@ -1,8 +1,17 @@
 package de.tohemi.justparty.test.junit;
 
+import de.tohemi.justparty.database.controller.DBLocationController;
+import de.tohemi.justparty.database.controller.DBUserController;
+import de.tohemi.justparty.datamodel.Address;
+import de.tohemi.justparty.datamodel.Location;
+import de.tohemi.justparty.datamodel.User;
+import de.tohemi.justparty.datamodel.exceptions.ZipCodeInvalidException;
+import de.tohemi.justparty.datamodel.wrapper.EMail;
+import de.tohemi.justparty.datamodel.wrapper.ZipCode;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.util.Assert;
 
 import static org.junit.Assert.*;
 
@@ -10,9 +19,17 @@ import static org.junit.Assert.*;
  * Created by xce35l2 on 28.04.2016.
  */
 public class DBLocationControllerTest {
-    @Before
-    public void setUp() throws Exception {
 
+    public DBLocationController conL;
+    public Location location;
+    public Address address;
+
+    @Before
+    public void setUp() throws Exception, ZipCodeInvalidException {
+        address = new Address("Teststraße", "12", new ZipCode(12345), "Testort", "Testland");
+        conL = new DBLocationController().getInstance();
+        location = new Location("LocationName", address, false);
+        conL.addLocation(location);
     }
 
     @After
@@ -22,32 +39,34 @@ public class DBLocationControllerTest {
 
     @Test
     public void getInstance() throws Exception {
-
+        Assert.notNull(DBLocationController.getInstance());
     }
 
     @Test
-    public void getLocationByID() throws Exception {
-
+    public void getLocationByID() throws Exception, ZipCodeInvalidException {
+        int id = conL.getLocationID(location);
+        Assert.isInstanceOf(Location.class, conL.getLocationByID(id));
     }
 
     @Test
     public void addLocation() throws Exception {
-
+        conL.deleteLocation(location);
+        Assert.isTrue(conL.addLocation(location));
     }
 
     @Test
     public void deleteLocation() throws Exception {
-
+        Assert.isTrue(conL.deleteLocation(location));
     }
 
     @Test
     public void updateLocation() throws Exception {
-
+        Assert.isTrue(conL.updateLocation(location));
     }
 
     @Test
     public void getLocationID() throws Exception {
-
+        Assert.notNull(conL.getLocationID(location));
     }
 
 }
