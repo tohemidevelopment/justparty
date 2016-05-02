cfe981d125c8aacfe979f3e33060bcc8b5e624e0
Tom433 <tom@wolske.tv>
2016-05-02 21:13:29 +0200

    - repair tests after merge, so that travis.io run correctly

--- src/test/java/de/tohemi/justparty/test/junit/DBLocationControllerTest.java
+++ src/test/java/de/tohemi/justparty/test/junit/DBLocationControllerTest.java
@@ -34,7 +34,7 @@ public class DBLocationControllerTest {
 
     @After
     public void tearDown() throws Exception {
-
+        conL.deleteLocation(location);
     }
 
     @Test
@@ -58,6 +58,7 @@ public class DBLocationControllerTest {
     public void deleteLocation() throws Exception {
         Assert.isTrue(conL.deleteLocation(location));
     }
+    //TODO: Unknown error with street, fix asap
     /*
     @Test
     public void updateLocation() throws Exception {
