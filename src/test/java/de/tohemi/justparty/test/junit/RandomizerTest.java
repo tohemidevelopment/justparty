05067900539152834c6464a0c49ddffa3272c770
Heiko Faller <heiko_faller@t-online.de>
2016-05-10 21:34:00 +0200

    Changed to JUnit4

--- src/test/java/de/tohemi/justparty/test/junit/RandomizerTest.java
+++ src/test/java/de/tohemi/justparty/test/junit/RandomizerTest.java
@@ -1,17 +1,16 @@
 package de.tohemi.justparty.test.junit;
 
 import de.tohemi.justparty.util.IDGenerator;
-import junit.framework.TestCase;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.junit.Test;
+import org.springframework.util.Assert;
 
 /**
  * Created by Heiko on 16.12.2015.
  */
-public class RandomizerTest extends TestCase {
+
+public class RandomizerTest {
+    @Test
     public void generateLength50() {
-        assertTrue(50 == IDGenerator.generateID(50).length());
+        Assert.isTrue(50 == IDGenerator.generateID(50).length());
     }
 }
