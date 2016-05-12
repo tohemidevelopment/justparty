5a56ef28f0a42b2fbc54d18a20686dc4aafed584
Tom433 <tom@wolske.tv>
2016-05-12 13:07:01 +0200

    - fix sonar bugs

--- src/main/java/de/tohemi/justparty/viewInterface/LogicalViewNames.java
+++ src/main/java/de/tohemi/justparty/viewInterface/LogicalViewNames.java
@@ -4,68 +4,64 @@ package de.tohemi.justparty.viewInterface;
  * Created by xce35l7 on 07.11.2015.
  */
 public class LogicalViewNames {
-    private final static String welcome = "welcomepage";
-    private final static String login = "login";
-    private final static String register = "register";
-    private final static String eventManager = "user/eventManager";
-    private final static String nameCreateEvent = "user/createEvent";
-    private final static String nameErrorPage = "error";
-    private final static String nameInfoPage = "infopage";
-    private final static String nameEditEvent = "user/editevent";
-    private final static String nameEventData = "user/eventdata";
-    private final static String nameAlertAnswerInvitaion = "fragments/alertanswerinvite";
-    private final static String nameShowGuestlist ="user/showguests";
-    private final static String nameVerifyEmail = "user/verifyEmail";
-    private final static String nameInviteUser ="user/inviteUsers";
+    private static final String WELCOME_PAGE = "welcomepage";
+    private static final String LOGIN = "LOGIN";
+    private static final String REGISTER = "REGISTER";
+    private static final String EVENTMANAGER = "user/EVENTMANAGER";
+    private static final String NAME_CREATE_EVENT = "user/createEvent";
+    private static final String NAME_ERROR_PAGE = "error";
+    private static final String NAME_INFO_PAGE = "infopage";
+    private static final String NAME_EDIT_EVENT = "user/editevent";
+    private static final String NAME_EVENT_DATA = "user/eventdata";
+    private static final String NAME_ALERT_ANSWER_INVITAION = "fragments/alertanswerinvite";
+    private static final String NAME_SHOW_GUESTLIST ="user/showguests";
+    private static final String NAME_VERIFY_EMAIL = "user/verifyEmail";
+    private static final String NAME_INVITE_USER ="user/inviteUsers";
 
     public static String getNameWelcomePage() {
-        return welcome;
+        return WELCOME_PAGE;
     }
 
     public static String getNameLogin() {
-        return login;
+        return LOGIN;
     }
 
-    public static String getNameRegister() {
-        return register;
-    }
+    public static String getNameRegister() { return REGISTER; }
 
-    public static String getNameEventManager() {
-        return eventManager;
-    }
+    public static String getNameEventManager() { return EVENTMANAGER; }
 
     public static String getNameCreateEvent() {
-        return nameCreateEvent;
+        return NAME_CREATE_EVENT;
     }
 
     public static String getNameErrorPage() {
-        return nameErrorPage;
+        return NAME_ERROR_PAGE;
     }
 
     public static String getNameInfoPage() {
-        return nameInfoPage;
+        return NAME_INFO_PAGE;
     }
 
     public static String getNameEditEvent() {
-        return nameEditEvent;
+        return NAME_EDIT_EVENT;
     }
 
     public static String getNameAlertAnswerInvitaion() {
-        return nameAlertAnswerInvitaion;
+        return NAME_ALERT_ANSWER_INVITAION;
     }
 
     public static String getNameShowGuestlist() {
-        return nameShowGuestlist;
+        return NAME_SHOW_GUESTLIST;
     }
 
     public static String getNameInviteUsers() {
-        return nameInviteUser;
+        return NAME_INVITE_USER;
     }
 
-    public static String getNameEventData() { return nameEventData; }
+    public static String getNameEventData() { return NAME_EVENT_DATA; }
 
     public static String getNameVerifyEmail() {
-        return nameVerifyEmail;
+        return NAME_VERIFY_EMAIL;
     }
 
 }
