92580001c3758528c09ea15d33bf9f92a70b30b9
Tom433 <tom@wolske.tv>
2016-05-13 19:06:14 +0200

    - fix sonar bugs

--- src/main/java/de/tohemi/justparty/datamodel/exceptions/ZipCodeInvalidException.java
+++ src/main/java/de/tohemi/justparty/datamodel/exceptions/ZipCodeInvalidException.java
@@ -4,7 +4,7 @@ package de.tohemi.justparty.datamodel.exceptions;
  * Created by Micha Piertzik on 12.11.2015.
  */
 public class ZipCodeInvalidException extends Exception {
-    public ZipCodeInvalidException() {
+    public ZipCodeInvalidException() {//no need of this
     }
 
     public ZipCodeInvalidException(String message) {
